f9de58cc938858181521fcc3052cd32067de2c58
John Erick Santos <johnsantosdev@gmail.com>
2022-10-16 05:52:37 +1100

    types for trip endpoint

--- types/trip-planner.d.ts
+++ types/trip-planner.d.ts
@@ -7,3 +7,25 @@ export type Trip = {
 };
 
 export type Departure = Pick<Trip, "originId" | "tripDate" | "tripTime">;
+
+export type StopItem = {
+    id: string;
+    disassembledName: string;
+    departureTimePlanned: string;
+    arrivalTimePlanned: string;
+};
+
+export type LegItem = {
+    transportation: {
+        number: string;
+        description: string;
+    };
+    stopSequence: Array<StopItem>;
+};
+
+export type JourneyItem = {
+    interchanges: number;
+    legs: Array<LegItem>;
+};
+
+export type Journeys = Array<JourneyItem>;
